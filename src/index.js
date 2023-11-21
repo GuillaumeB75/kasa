0626418ce54b3023dfb38265c98363ba95dcdd1f
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-11-21 13:06:19 +0100

    ajout du "header" dans l'indexation

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import RoutesDefinition from "./routes/RoutesConfiguration";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    
     <Router>
+    <Header />
       <RoutesDefinition />
     </Router>
     <Footer />
