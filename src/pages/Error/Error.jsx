39e3bf7b1e5501b1c321eb1015e1aa0b5363c53a
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-11-24 12:06:18 +0100

    ajout du "navlink" dans "error.jsx" pour retour page "accueil"

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./error.scss";
+import { NavLink } from "react-router-dom";
 
 function Error() {
   return (
     <div className="error_page">
       <h1>404</h1>
       <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <NavLink to="/">
       <h3>Retourner sur la page d'accueil</h3>
+      </NavLink>
     </div>
   );
 }
