923ad2255c94220a542c1c914801dcefbd0f4389
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-11-27 23:47:44 +0100

    essai banner texte

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
 import backgroundImage from "../../assets/images/banniere_home.png";
+import "./home.scss";
+
 
 function Home() {
   return (
-    <div className="test">
+    <div className="home_presentation">
       <Banner backgroundImage={backgroundImage} />
+      <div className="home_title">
+        <p>Chez vous, partout et ailleurs</p>
+      </div>
       <h1>home</h1>
-      
     </div>
   );
 }
