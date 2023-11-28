725f532822bdb48ab03951cb6d68c78fd8651f90
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-11-28 13:36:16 +0100

    réalsisation du component "collapse" en première version ( à modifier)

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./collapse.scss";
+
+const CollapseSection = ({ title, children }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleOpen = () => setOpen(!isOpen);
+
+  return (
+    <div className="Collapse-section">
+      <button className="Collapse-title" onClick={toggleOpen}>
+        {title}
+        <span className={`Collapse-icon ${isOpen ? "open" : ""}`}>^</span>
+      </button>
+      {isOpen && <div className="Collapse-content">{children}</div>}
+    </div>
+  );
+};
+
+const Collapse = () => {
+  return (
+    <div className="Collapse">
+      <CollapseSection title="Fiabilité">
+        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+        photos sont conformes aux logements, et toutes les informations sont
+        régulièrement vérifiées par nos équipes.
+      </CollapseSection>
+      <CollapseSection title="Respect">
+        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+        comportement discriminatoire ou de perturbation du voisinage entraînera
+        une exclusion de notre plateforme.
+      </CollapseSection>
+      <CollapseSection title="Service">
+        Nos équipes se tiennent à votre disposition pour vous fournir une
+        expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+        moindre question.
+      </CollapseSection>
+      <CollapseSection title="Sécurité">
+        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
+        les voyageurs, chaque logement correspond aux critères de sécurité
+        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+        locataire, cela permet à nos équipes de vérifier que les standards sont
+        bien respectés. Nous organisons également des ateliers sur la sécurité
+        domestique pour nos hôtes.
+      </CollapseSection>
+    </div>
+  );
+};
+
+export default Collapse;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
 import backgroundImage from "../../assets/images/banniere_about.png";
+import Collapse from "../../components/Collapse/Collapse";
 
 
 function About() {
   return (
     <div className="about">
       <Banner backgroundImage={backgroundImage} />
-      <h1>a propos</h1>
+      <Collapse />
+      
       
     </div>
   );
