fc6c45a7c4f5fbc4745f0d89209a8098f063e163
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-12-26 13:45:54 +0100

    corrections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,11 +9,9 @@ function Home() {
   return (
     <div className="home_presentation">
       <Banner backgroundImage={backgroundImage} />
-      <div className="home_title">
-        <p>Chez vous, partout et ailleurs</p>
-      </div>
+     
       <Gallery />
-      <h1>home</h1>
+      
     </div>
   );
 }
