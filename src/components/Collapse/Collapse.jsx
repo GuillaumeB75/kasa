1d8d48c36d3acb9c80fcffcc20748c531a30ebe9
GuillaumeB75 <guillaume.bezie@yahoo.com>
2024-01-02 11:51:43 +0100

    création fichierabout.json et corrections majeurs collapse .jsx

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./collapse.scss";
 
-const CollapseSection = ({ title, children }) => {
+function Collapse ({ title, content }) {
   const [isOpen, setOpen] = useState(false);
 
   const toggleOpen = () => setOpen(!isOpen);
@@ -12,39 +12,10 @@ const CollapseSection = ({ title, children }) => {
         {title}
         <span className={`Collapse-icon ${isOpen ? 'open' : ''}`}>^</span>
       </button>
-      <div className="Collapse-content">{children}</div>
+      <div className="Collapse-content">{content}</div>
     </div>
   );
 };
 
-const Collapse = () => {
-  return (
-    <div className="Collapse">
-      <CollapseSection title="Fiabilité">
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-        photos sont conformes aux logements, et toutes les informations sont
-        régulièrement vérifiées par nos équipes.
-      </CollapseSection>
-      <CollapseSection title="Respect">
-        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-        comportement discriminatoire ou de perturbation du voisinage entraînera
-        une exclusion de notre plateforme.
-      </CollapseSection>
-      <CollapseSection title="Service">
-        Nos équipes se tiennent à votre disposition pour vous fournir une
-        expérience parfaite. N'hésitez pas à nous contacter si vous avez la
-        moindre question.
-      </CollapseSection>
-      <CollapseSection title="Sécurité">
-        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
-        les voyageurs, chaque logement correspond aux critères de sécurité
-        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-        locataire, cela permet à nos équipes de vérifier que les standards sont
-        bien respectés. Nous organisons également des ateliers sur la sécurité
-        domestique pour nos hôtes.
-      </CollapseSection>
-    </div>
-  );
-};
 
 export default Collapse;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import Banner from "../../components/Banner/Banner";
 import backgroundImage from "../../assets/images/banniere_about.png";
 import Collapse from "../../components/Collapse/Collapse";
+import about from "../../assets/data/about.json";
 
 
 function About() {
   return (
+    <>
+    <Banner backgroundImage={backgroundImage} />
     <div className="about">
-      <Banner backgroundImage={backgroundImage} />
-      <Collapse />
+      {about.map((house) => (
+          <Collapse
+            key={house.id}
+            title={house.title}
+            content={house.description}
+          />
+        ))}
       
       
     </div>
+    </>
   );
 }
 
